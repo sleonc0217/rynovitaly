93aee1c483ff51aef5e51fe158d29e82718efad7
jim <jim29sozasancho@gmail.com>
2022-11-20 22:03:19 -0600

    Internacion
    Internacion

--- rynovitaly/src/main/java/com/rynovitaly/controller/IndexController.java
+++ rynovitaly/src/main/java/com/rynovitaly/controller/IndexController.java
@@ -2,6 +2,7 @@
 package com.rynovitaly.controller;
 
 
+import com.rynovitaly.dao.UsuarioDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,14 +13,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class IndexController {
     
-    
+   @Autowired
+   private UsuarioDao usuarioDao;
    
     
     
     @GetMapping("/")
-    public String inicio(){
-    
+    public String inicio(Model model){
+        var usuarios = usuarioDao.findAll();
         
+     model.addAttribute("usuarios",usuarios);
         
         
  
